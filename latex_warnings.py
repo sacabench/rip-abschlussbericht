22c47c39af0fa1c5855307a2a73eb9b6bd59f9aa
Marvin Löbel <loebel.marvin@gmail.com>
2019-03-04 01:30:45 +0100

    handle latex make warnings better

--- latex_warnings.py
+++ latex_warnings.py
@@ -6,14 +6,19 @@ import subprocess
 import re
 
 re_warning = re.compile("Warning|Error")
-re_path = re.compile("\\./.*?\\.tex")
+re_path = re.compile("(\\./.*?\\.(pygtex|pygstyle|tex|pdf|png|toc|sty|w18))")
 
 cmd = sys.argv[1:]
+
+print("Wrapper script executes: {}".format(cmd))
+
+env = os.environ.copy()
+env["max_print_line"] = "10000"
 
 process = subprocess.Popen(cmd,
                            stdout=subprocess.PIPE,
                            stderr=subprocess.STDOUT,
-                           env={ "max_print_line": "10000" })
+                           env=env)
 
 output = []
 for line in iter(process.stdout.readline, b''):
@@ -24,11 +29,21 @@ for line in iter(process.stdout.readline, b''):
 process.stdout.close()
 returncode = process.wait()
 
+print("---Warnings and Errors ---------------------------------------------------------")
+
+last_file = None
+current_file = "asdf"
 for line in output:
     for m in re_path.findall(line):
-        print("File {}".format(m))
+        last_file_candidate = "File {}".format(m[0])
+        #print("[" + last_file_candidate + "]")
+        if last_file_candidate.endswith(".tex"):
+            last_file = last_file_candidate
 
-    if re_warning.match(line):
-        print(line.strip()),
+    if re_warning.search(line):
+        if current_file != last_file:
+            print(last_file)
+            current_file = last_file
+        print("  " + line.strip()),
 
 exit(returncode)
--- makefile
+++ makefile
@@ -1,9 +1,9 @@
-TEX = latexmk
+TEX = ./latex_warnings.py latexmk
 
 all : pdf
 
 pdf :
-	max_print_line=10000 $(TEX) -pdf main.tex 2>&1 | tee latexmk_output.log && cat latexmk_output.log | grep --color=auto -E "Warning|Missing" || true
+	$(TEX) -pdf main.tex
 
 run :
 	$(TEX) -pdf -pv main.tex
