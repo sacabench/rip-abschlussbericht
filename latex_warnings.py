818550ab33684cd22ecd16583cccc2fbb3dde8f6
Marvin Löbel <loebel.marvin@gmail.com>
2019-03-04 12:20:57 +0100

    detect todos

--- latex_warnings.py
+++ latex_warnings.py
@@ -8,6 +8,9 @@ import re
 re_warning = re.compile("Warning|Error")
 re_path = re.compile("(\\./.*?\\.(pygtex|pygstyle|tex|pdf|png|toc|sty|w18))")
 
+todo_words = ["TODO", "FIXME", "todo", "fixme", "Todo", "Fixme"]
+re_todo = re.compile("|".join(todo_words))
+
 cmd = sys.argv[1:]
 
 print("Wrapper script executes: {}".format(cmd))
@@ -84,18 +87,31 @@ def colorize(text, colorcode):
 last_file = None
 current_file = "asdf"
 for line in output:
+    def print_warning(warn_text):
+        global last_file
+        global current_file
+
+        if current_file != last_file:
+            print("File {}".format(colorize(last_file, CGREEN)))
+            current_file = last_file
+        print("  " + warn_text.strip())
+
     for m in re_path.findall(line):
         last_file_candidate = str(m[0])
         #print("[" + last_file_candidate + "]")
         if last_file_candidate.endswith(".tex"):
             last_file = last_file_candidate
+            if os.path.isfile(last_file):
+                with open(last_file, 'r') as f:
+                    for i,line2 in enumerate(f.readlines()):
+                        if re_todo.search(line2):
+                            for todo_word in todo_words:
+                                line2 = line2.replace(todo_word, colorize(todo_word, CVIOLET))
+                            print_warning("{} on line {}: {}".format(colorize("Todo", CVIOLET), i, line2))
 
     if re_warning.search(line):
-        if current_file != last_file:
-            print("File {}".format(colorize(last_file, CGREEN)))
-            current_file = last_file
         line = line.replace("Warning", colorize("Warning", CYELLOW))
         line = line.replace("Error", colorize("Error", CRED))
-        print("  " + line.strip()),
+        print_warning(line)
 
 exit(returncode)
